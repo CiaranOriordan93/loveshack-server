566e5cbab80979198ed5a168447d80880c6de880
ciaranoriordan93 <ciaranoriordan93@gmail.com>
2020-07-13 13:37:40 +0100

    moved cors lexically

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
-app.use(bodyParer.json());
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+app.use(bodyParer.json());
+
 
 app.use('/admin', adminRoutes);
 
