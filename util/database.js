237ba6928a5d659a91e558320d6aa2fb6ae45621
ciaranoriordan93 <ciaranoriordan93@gmail.com>
2020-07-13 14:54:02 +0100

    test

--- util/database.js
+++ util/database.js
@@ -4,10 +4,9 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://CiaranO:K3Y5EmxaQpxirqoh@cluster0.vmnvr.mongodb.net/prices?retryWrites=true&w=majority')
+    MongoClient.connect('MONGODB_URI')
         .then(client => {
             _db = client.db();
-            callback();
     }).catch(error => {
         console.log(error);
         throw error;
