50de608c88fe1329d64532fc0151352d6655d712
ciaranoriordan93 <ciaranoriordan93@gmail.com>
2020-07-13 15:06:44 +0100

    w

--- util/database.js
+++ util/database.js
@@ -4,10 +4,10 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = callback => {
-    console.log(process.env.MONGODB_URI)
     MongoClient.connect(process.env.MONGODB_URI)
         .then(client => {
-            _db = client.db();
+            _db = client.db()
+            .callback();
     }).catch(error => {
         console.log(error);
         throw error;
