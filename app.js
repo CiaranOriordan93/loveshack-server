e6124f5c00c060c0abe48265f3feec60f1b6cbe7
ciaranoriordan93 <ciaranoriordan93@gmail.com>
2020-07-13 14:56:01 +0100

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const adminRoutes = require('./routes/admin');
 const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
-const PORT = process.nextTick.PORT || 3003
+const PORT = process.env.PORT || 3003;
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
